6aa4fed77ddfd2cad4744dbfa9ff7f043efc0ce4
a.bogdanov <a.bogdanov@netex.pro>
2020-10-14 18:26:09 +0300

    godspeed 5x14 kb

---
+++ keyboards/godspeed/config.h
@@ -0,0 +1,37 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#include "config_common.h"
+
+/* USB Device descriptor parameter */
+#define VENDOR_ID       0xFEED
+#define PRODUCT_ID      0x6060
+#define DEVICE_VER      0x0120
+#define MANUFACTURER    BogdanovArt
+#define PRODUCT         Godspeed
+#define DESCRIPTION     Ortholinear 5x14 keyboard
+
+/* key matrix size */
+#define MATRIX_ROWS 5
+#define MATRIX_COLS 14
+
+/* key matrix pins */
+#define MATRIX_ROW_PINS { F6, F5, F4, F1, F0 }
+#define MATRIX_COL_PINS { D3, D2, D1, D0, B7, B3, B2, B1, B0, F7, B6, B5, B4, D7 }
+#define UNUSED_PINS
+
+/* COL2ROW or ROW2COL */
+#define DIODE_DIRECTION COL2ROW
+
+#define TAPPING_TERM 200
+
+/* Set 0 if debouncing isn't needed */
+#define DEBOUNCE 5
+
+/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
+#define LOCKING_SUPPORT_ENABLE
+
+/* Locking resynchronize hack */
+#define LOCKING_RESYNC_ENABLE
+
+#endif
---
+++ keyboards/godspeed/rules.mk
@@ -0,0 +1,12 @@
+# MCU name
+MCU = atmega32u4
+
+BOOTLOADER = halfkay
+
+# Build Options
+#   comment out to disable the options.
+#
+
+EXTRAKEY_ENABLE = yes		# Audio control and System control(+450)
+NKRO_ENABLE = yes			# USB Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
+COMBO_ENABLE = yes
